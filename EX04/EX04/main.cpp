584f51a9ab18f2641cfa75942f9e8fec4969b389
Josiah Gallegos <jgallegos20@my.whitworth.edu>
2017-06-11 13:15:38 -0700

    EX04_01 complete

--- EX04/EX04/main.cpp
+++ EX04/EX04/main.cpp
@@ -8,6 +8,34 @@
 
 #include <iostream>
 
+using namespace std;
+
+void EX04_01(){
+    cout<<"Input a size for your array: ";
+    int input;
+    cin>>input;
+    int numbers[input];
+    cout<<"Now enter your "<<input<<" numbers."<<endl;
+    int input2;
+    for (int i = 0; i<input; i++){
+        cin>>input2;
+        numbers[i] = input2;
+    }
+    int sum = 0;
+    double avg = 0;;
+    for (int i = 0; i<input; i++){
+        sum += numbers[i];
+        avg = sum/input;
+    }
+    cout<<"The average of these numbers is "<<avg<<endl;
+    int count = 0;
+    for (int i = 0; i<input; i++){
+        if (numbers[i]>avg){
+            count++;
+        }
+    }
+    cout<<"There are "<<count<<" numbers above the average of "<<avg<<endl;
+}
 int main(){
-    
+    EX04_01();
 }
